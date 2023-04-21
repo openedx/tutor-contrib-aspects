d9617962e4bca9f65a02847570deafda48b4e67f
Cristhian Garcia <crisgarta8@gmail.com>
2023-04-21 10:49:09 -0500

    feat: add k8s related files

--- tutoroars/templates/oars/jobs/init/superset-api-dashboard.sh
+++ tutoroars/templates/oars/jobs/init/superset-api-dashboard.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-VENV_DIR=/opt/venv
-
-python -m venv $VENV_DIR
-$VENV_DIR/bin/pip install -r /app/oars/pythonpath/requirements.txt
-$VENV_DIR/bin/python /app/oars/pythonpath/superset-dashboard.py && echo "Success"
+pip install -r /app/oars/pythonpath/requirements.txt
+python /app/oars/pythonpath/superset-dashboard.py && echo "Success"
