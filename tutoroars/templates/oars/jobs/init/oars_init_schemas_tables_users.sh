7d83ace647ca3868239f975a5cf4c48b2509ddaa
Brian Mesick <bmesick@tcril.org>
2023-05-11 10:24:54 -0400

    fix: Update course_overviews table
    Updates to make the table structure match the current implementation.

--- tutoroars/templates/oars/jobs/init/oars_init_schemas_tables_users.sh
+++ tutoroars/templates/oars/jobs/init/oars_init_schemas_tables_users.sh
@@ -66,30 +66,33 @@ CREATE DATABASE IF NOT EXISTS {{ OARS_EVENT_SINK_DATABASE }};
 
 CREATE TABLE IF NOT EXISTS {{ OARS_EVENT_SINK_DATABASE }}.{{ OARS_EVENT_SINK_OVERVIEWS_TABLE }}
 (
-    org              String NOT NULL,
-    course_key       String NOT NULL,
-    location         String NOT NULL,
-    display_name     String NOT NULL,
-    xblock_data_json String NOT NULL,
-    order            Int32 default 0,
-    dump_id          UUID NOT NULL,
-    time_last_dumped String NOT NULL,
-    created          String NOT NULL,
-    modified         String NOT NULL
+    org String NOT NULL,
+    course_key String NOT NULL,
+    display_name String NOT NULL,
+    course_start String NOT NULL,
+    course_end String NOT NULL,
+    enrollment_start String NOT NULL,
+    enrollment_end String NOT NULL,
+    self_paced BOOL NOT NULL,
+    course_data_json String NOT NULL,
+    created String NOT NULL,
+    modified String NOT NULL,
+    dump_id UUID NOT NULL,
+    time_last_dumped String NOT NULL
 ) engine = MergeTree
-    PRIMARY KEY (org, course_key, location, modified)
-    ORDER BY (org, course_key, location, modified, order);
+    PRIMARY KEY (org, course_key, modified, time_last_dumped)
+    ORDER BY (org, course_key, modified, time_last_dumped);
 
 CREATE TABLE IF NOT EXISTS {{ OARS_EVENT_SINK_DATABASE }}.{{ OARS_EVENT_SINK_NODES_TABLE }}
 (
-    org              String NOT NULL,
-    course_key       String NOT NULL,
-    location         String NOT NULL,
-    display_name     String NOT NULL,
+    org String NOT NULL,
+    course_key String NOT NULL,
+    location String NOT NULL,
+    display_name String NOT NULL,
     xblock_data_json String NOT NULL,
-    order            Int32 default 0,
-    edited_on        String NOT NULL,
-    dump_id          UUID NOT NULL,
+    order Int32 default 0,
+    edited_on String NOT NULL,
+    dump_id UUID NOT NULL,
     time_last_dumped String NOT NULL
 ) engine = MergeTree
     PRIMARY KEY (org, course_key, location, edited_on)
@@ -97,11 +100,11 @@ CREATE TABLE IF NOT EXISTS {{ OARS_EVENT_SINK_DATABASE }}.{{ OARS_EVENT_SINK_NOD
 
 CREATE TABLE IF NOT EXISTS {{ OARS_EVENT_SINK_DATABASE }}.{{ OARS_EVENT_SINK_RELATIONSHIPS_TABLE }}
 (
-    course_key       String NOT NULL,
-    parent_location  String NOT NULL,
-    child_location   String NOT NULL,
-    order            Int32 NOT NULL,
-    dump_id          UUID NOT NULL,
+    course_key String NOT NULL,
+    parent_location String NOT NULL,
+    child_location String NOT NULL,
+    order Int32 NOT NULL,
+    dump_id UUID NOT NULL,
     time_last_dumped String NOT NULL
 ) engine = MergeTree
     PRIMARY KEY (course_key, parent_location, child_location, order)
