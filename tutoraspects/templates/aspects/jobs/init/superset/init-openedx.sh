282edf10c1dd77846a235d425bf8df910246840d
Cristhian Garcia <crisgarta8@gmail.com>
2023-06-23 13:17:15 -0500

    fix: enable superset beat, fix local sso

--- tutoraspects/templates/aspects/jobs/init/superset/init-openedx.sh
+++ tutoraspects/templates/aspects/jobs/init/superset/init-openedx.sh
@@ -2,9 +2,17 @@
 ./manage.py lms manage_user superset superset@apache
 ./manage.py lms create_dot_application \
     --grant-type authorization-code \
-    --redirect-uris "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ SUPERSET_HOST }}{% if not ENABLE_HTTPS %}:{{SUPERSET_PORT}}{% endif %}/oauth-authorized/openedxsso" \
+    --redirect-uris "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ SUPERSET_HOST }}/oauth-authorized/openedxsso" \
     --client-id {{ SUPERSET_OAUTH2_CLIENT_ID }} \
     --client-secret {{ SUPERSET_OAUTH2_CLIENT_SECRET }} \
     --scopes "user_id" \
     --update \
     superset-sso superset
+./manage.py lms create_dot_application \
+    --grant-type authorization-code \
+    --redirect-uris "http://{{ SUPERSET_HOST }}:{{SUPERSET_PORT}}/oauth-authorized/openedxsso" \
+    --client-id {{ SUPERSET_OAUTH2_CLIENT_ID_DEV }} \
+    --client-secret {{ SUPERSET_OAUTH2_CLIENT_SECRET }} \
+    --scopes "user_id" \
+    --update \
+    superset-sso-dev superset
