2d649d19fcac1784195e1993928481bf253cb526
Brian Mesick <bmesick@tcril.org>
2023-06-28 13:13:15 -0400

    refactor: Move env vars, de-dupe dbt commands

--- tutoraspects/templates/aspects/jobs/init/dbt/init-dbt.sh
+++ tutoraspects/templates/aspects/jobs/init/dbt/init-dbt.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-python3 -m venv virtualenv
-. virtualenv/bin/activate
-
-echo "Installing dbt packages..."
-
-pip install -r /app/aspects/dbt/requirements.txt
-
-echo "Installing aspects-dbt"
-git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}
-
-cd {{ DBT_REPOSITORY_PATH }} || exit
-
-echo "Installing dbt dependencies"
-dbt deps --profiles-dir /app/aspects/dbt/
-
-echo "Running dbt"
-XAPI_SCHEMA={{ ASPECTS_XAPI_DATABASE }} ASPECTS_ENROLLMENT_EVENTS_TABLE={{ ASPECTS_ENROLLMENT_EVENTS_TABLE }} dbt run --profiles-dir /app/aspects/dbt/
+# Make the script executable
+chmod +x /app/aspects/scripts/dbt.sh
+/app/aspects/scripts/dbt.sh run
