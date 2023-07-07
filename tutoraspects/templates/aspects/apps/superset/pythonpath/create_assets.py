339ed92ba99648cff6696ab9d66d4c58d5d60468
Cristhian Garcia <crisgarta8@gmail.com>
2023-07-07 11:23:29 -0500

    refactor: remove assets zip

--- tutoraspects/templates/aspects/apps/superset/pythonpath/create_assets.py
+++ tutoraspects/templates/aspects/apps/superset/pythonpath/create_assets.py
@@ -70,7 +70,8 @@ def create_assets():
                     break
 
     # Create the zip file and import the assets
-    with ZipFile("/app/pythonpath/assets.zip", "w") as zip:
+    zip_path = "/app/pythonpath/assets.zip"
+    with ZipFile(zip_path, "w") as zip:
         for folder in ASSET_FOLDER_MAPPING.values():
             for file_name in os.listdir(f"{BASE_DIR}/{folder}"):
                 zip.write(f"{BASE_DIR}/{folder}/{file_name}", f"import/{folder}/{file_name}")
@@ -82,6 +83,8 @@ def create_assets():
         )
 
         command.run()
+
+    os.remove(zip_path)
     
     # Create the roles
     for dashboard_uuid, role_ids in roles.items():
