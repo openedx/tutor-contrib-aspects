11ae1c72115c8eb61ff3419380ea66caee321294
Cristhian Garcia <crisgarta8@gmail.com>
2023-07-14 14:57:13 -0500

    fix: add alembic compatibility with k8s

--- tutoraspects/templates/aspects/jobs/init/aspects/init-aspects.sh
+++ tutoraspects/templates/aspects/jobs/init/aspects/init-aspects.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-chmod +x /app/aspects/scripts/alembic.sh
 /app/aspects/scripts/alembic.sh upgrade head
 
-chmod +x /app/aspects/scripts/dbt.sh
 /app/aspects/scripts/dbt.sh run
