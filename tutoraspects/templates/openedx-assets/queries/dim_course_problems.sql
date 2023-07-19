67a951c287d74156273ced157e508185c4b691ff
Brian Mesick <bmesick@tcril.org>
2023-07-19 09:19:11 -0400

    fix: Wrap SQL "in" lists in parens
    Superset was complaining about the missing parentheses on import. I don't know that it impacted functionality, but it was easy to fix.

--- tutoraspects/templates/openedx-assets/queries/dim_course_problems.sql
+++ tutoraspects/templates/openedx-assets/queries/dim_course_problems.sql
@@ -12,10 +12,10 @@ with courses as (
         JSON_VALUE(xblock_data_json, '$.block_type') = 'problem'
     {% raw -%}
         {% if filter_values('org') != [] %}
-        and org in {{ filter_values('org') | where_in }}
+        and org in ({{ filter_values('org') | where_in }})
         {% endif %}
         {% if filter_values('problem_name') != [] %}
-        and problem_name in {{ filter_values('problem_name') | where_in }}
+        and problem_name in ({{ filter_values('problem_name') | where_in }})
         {% endif %}
     {%- endraw %}
 )
