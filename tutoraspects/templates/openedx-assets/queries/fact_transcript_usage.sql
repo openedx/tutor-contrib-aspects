ab066a53b2c4f6d34a9c841ac0bb2907af9d42dc
Rory Sawyer <rory@sawyer.dev>
2023-07-25 14:29:22 -0400

    refactor!: parse course key in MV queries (FC-0024) (#193)
    Adds an alembic migration to update the materialized view queries. The
    course key is parsed from the `course_id` field in order to match the data
    in the `event_sink` tables.
    
    Drops existing tables and materialized view definitions and replaces
    them with new tables and queries that parse the course key out of the
    course url
    
    Use ReplacingMergeTree for top-level MVs to facilitate deduplication

--- tutoraspects/templates/openedx-assets/queries/fact_transcript_usage.sql
+++ tutoraspects/templates/openedx-assets/queries/fact_transcript_usage.sql
@@ -4,7 +4,7 @@ with videos as (
     select
         emission_time,
         org,
-        splitByString('/', course_id)[-1] as course_key,
+        course_key,
         video_id,
         actor_id
     from
