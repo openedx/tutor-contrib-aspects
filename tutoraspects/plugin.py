b52199ed2288275fb3b87968f9d6f53767dc4199
Rory Sawyer <rory@sawyer.dev>
2023-08-21 13:49:19 -0400

    chore: update aspects-dbt version

--- tutoraspects/plugin.py
+++ tutoraspects/plugin.py
@@ -214,7 +214,7 @@ hooks.Filters.CONFIG_DEFAULTS.add_items(
         # For now we are pulling this from github, which should allow maximum
         # flexibility for forking, running branches, specific versions, etc.
         ("DBT_REPOSITORY", "https://github.com/openedx/aspects-dbt"),
-        ("DBT_BRANCH", "v2.0.0"),
+        ("DBT_BRANCH", "v2.1"),
         # Path to the dbt project inside the repository
         ("DBT_REPOSITORY_PATH", "aspects-dbt/aspects"),
         # This is a pip compliant list of Python packages to install to run dbt
