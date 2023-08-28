e7cf50e1fa88897aa442a81554f524bf9c96876b
Cristhian Garcia <crisgarta8@gmail.com>
2023-08-28 15:11:39 -0500

    fix: remove unused dbt overrides

--- tutoraspects/templates/aspects/jobs/init/aspects/init-aspects.sh
+++ tutoraspects/templates/aspects/jobs/init/aspects/init-aspects.sh
@@ -16,17 +16,13 @@ echo "Installing dbt packages..."
 
 pip install -r /app/aspects/dbt/requirements.txt
 
-rm -rf aspects-dbt
+rm -rf {{ DBT_REPOSITORY_PATH }}
 
 echo "Installing aspects-dbt"
+echo "git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}"
 git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}
 
 cd {{ DBT_REPOSITORY_PATH }} || exit
-
-{% if DBT_ENABLE_OVERRIDE %}
-cat /app/aspects/dbt/packages.yml > packages.yml
-cat /app/aspects/dbt/dbt_project.yml > dbt_project.yml
-{% endif %}
 
 export ASPECTS_EVENT_SINK_DATABASE={{ASPECTS_EVENT_SINK_DATABASE}}
 
