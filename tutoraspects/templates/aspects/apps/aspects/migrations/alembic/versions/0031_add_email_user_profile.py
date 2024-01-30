81b2f8b18529bd71a1188b0d24330cd844b78aee
Brian Mesick <bmesick@tcril.org>
2024-01-30 10:07:00 -0500

    feat: Upgrade event-sink-clickhouse to 1.0.0
    Adds email to the user_profile table.

---
+++ tutoraspects/templates/aspects/apps/aspects/migrations/alembic/versions/0031_add_email_user_profile.py
@@ -0,0 +1,32 @@
+"""
+Add email column to user profile table
+"""
+from alembic import op
+
+
+revision = "0031"
+down_revision = "0030"
+branch_labels = None
+depends_on = None
+on_cluster = " ON CLUSTER '{{CLICKHOUSE_CLUSTER_NAME}}' " if "{{CLICKHOUSE_CLUSTER_NAME}}" else ""
+
+
+def upgrade():
+    op.execute(
+        f"""
+        ALTER TABLE {{ ASPECTS_EVENT_SINK_DATABASE }}.{{ ASPECTS_EVENT_SINK_USER_PROFILE_TABLE }} 
+        {on_cluster}
+        ADD COLUMN IF NOT EXISTS email String DEFAULT '' 
+        AFTER name;
+        """
+    )
+
+
+def downgrade():
+    op.execute(
+        f"""
+        ALTER TABLE {{ ASPECTS_EVENT_SINK_DATABASE }}.{{ ASPECTS_EVENT_SINK_USER_PROFILE_TABLE }} 
+        {on_cluster}
+        DROP COLUMN IF EXISTS email;
+        """
+    )
