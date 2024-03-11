d6369e14dc3ce29002b6a1bd641cb4fd85b4bcad
Cristhian Garcia <crisgarta8@gmail.com>
2024-03-11 11:40:51 -0500

    chore: enable superset hot-reload

--- tutoraspects/templates/aspects/apps/superset/docker/docker-bootstrap.sh
+++ tutoraspects/templates/aspects/apps/superset/docker/docker-bootstrap.sh
@@ -47,7 +47,7 @@ elif [[ "${1}" == "beat" ]]; then
   celery --app=superset.tasks.celery_app:app beat --pidfile /tmp/celerybeat.pid -l INFO -s "${SUPERSET_HOME}"/celerybeat-schedule
 elif [[ "${1}" == "app" ]]; then
   echo "Starting web app..."
-  flask run -p 8088 --with-threads --reload --debugger --host=0.0.0.0
+  flask run -p 8088 --with-threads --reload --debugger --debug --host=0.0.0.0
 elif [[ "${1}" == "app-gunicorn" ]]; then
   echo "Starting web app..."
   /usr/bin/run-server.sh
