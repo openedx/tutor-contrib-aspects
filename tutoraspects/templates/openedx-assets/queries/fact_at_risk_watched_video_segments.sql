7b3a4cfae4ead0acb356b83d889f63808598dc69
rory sawyer <rory@sawyer.dev>
2024-05-07 16:48:22 -0400

    feat: add grade distribution chart to at-risk dashboard

--- tutoraspects/templates/openedx-assets/queries/fact_at_risk_watched_video_segments.sql
+++ tutoraspects/templates/openedx-assets/queries/fact_at_risk_watched_video_segments.sql
@@ -1,8 +1,4 @@
-with segment_watches as (
-  {% include 'openedx-assets/queries/fact_watched_video_segments.sql' %}
-)
-
-select segment_watches.*
-from segment_watches
+select watches.*
+from {{ DBT_PROFILE_TARGET_DATABASE}}.fact_watched_video_segments watches
 join {{ DBT_PROFILE_TARGET_DATABASE }}.dim_at_risk_learners
 using (org, course_key, actor_id)
