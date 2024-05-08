5d4ba6bfa84ebf43dedcea614039628da4a40bbe
Cristhian Garcia <crisgarta8@gmail.com>
2024-05-08 12:03:12 -0500

    fix: update fields to latest dbt changes
    (cherry picked from commit 6959c43d60934f9eaf09fca91e165a6d0e96034f)

--- tutoraspects/templates/openedx-assets/queries/fact_at_risk_pageview_engagement.sql
+++ tutoraspects/templates/openedx-assets/queries/fact_at_risk_pageview_engagement.sql
@@ -1,8 +1,4 @@
-with engagement as (
-  {% include 'openedx-assets/queries/fact_pageview_engagement.sql' %}
-)
-
 select *
-from engagement
+from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_pageview_engagement
 join {{ DBT_PROFILE_TARGET_DATABASE }}.dim_at_risk_learners
 using (org, course_key, actor_id)
