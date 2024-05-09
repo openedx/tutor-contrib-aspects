11138050e71fefef26400ed9e84cbbb7bff0c39a
Cristhian Garcia <cristhian.garcia@edunext.co>
2024-05-09 09:01:17 -0500

    fix: use proper dataset columns (#785)
    * fix: only omit template vars for dictionaries
    
    * feat: upgrade dbt
    
    * fix: add missing verbose name for datasets
    
    * fix: update fields to latest dbt changes
    
    * fix: only select fact_video_engagement fields for at risk datasets
    
    * chore: update engagement datasets columns
    
    * chore: Bump aspects_dbt to 3.24.0
    
    * feat: import new individual learner dashboard (#786)

--- tutoraspects/templates/openedx-assets/queries/fact_at_risk_pageview_engagement.sql
+++ tutoraspects/templates/openedx-assets/queries/fact_at_risk_pageview_engagement.sql
@@ -1,8 +1,5 @@
-with engagement as (
-  {% include 'openedx-assets/queries/fact_pageview_engagement.sql' %}
-)
-
-select *
-from engagement
+select
+    fact_pageview_engagement.*
+from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_pageview_engagement
 join {{ DBT_PROFILE_TARGET_DATABASE }}.dim_at_risk_learners
 using (org, course_key, actor_id)
