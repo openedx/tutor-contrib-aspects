07a5ca272a30fe1999bb798241f46fcb8ea5052d
Cristhian Garcia <crisgarta8@gmail.com>
2024-05-23 15:18:30 -0500

    feat: upgrade aspects-dbt to v3.26.0
    feat: upgrade aspects-dbt to v3.26.0

--- tutoraspects/templates/openedx-assets/queries/fact_at_risk_problem_engagement.sql
+++ tutoraspects/templates/openedx-assets/queries/fact_at_risk_problem_engagement.sql
@@ -2,4 +2,8 @@
 join (
     {% include 'openedx-assets/queries/at_risk_learner_filter.sql' %}
 ) as at_risk_learners
-using (org, course_key, actor_id)
+on (
+    pe.org = at_risk_learners.org
+    and pe.course_key = at_risk_learners.course_key
+    and pe.actor_id = at_risk_learners.actor_id
+)
