408424d71e83780c81f09c3294247ffbbd2f2596
Cristhian Garcia <crisgarta8@gmail.com>
2024-06-27 15:02:03 -0500

    fix: create functions on cluster
    fix: create functions on cluster
    
    fix: create functions on cluster

--- tutoraspects/templates/aspects/apps/aspects/migrations/alembic/versions/0001_get_org_from_course_url.py
+++ tutoraspects/templates/aspects/apps/aspects/migrations/alembic/versions/0001_get_org_from_course_url.py
@@ -19,7 +19,7 @@ def upgrade():
     """
     op.execute(
         f"""
-        CREATE OR REPLACE FUNCTION get_org_from_course_url
+        CREATE OR REPLACE FUNCTION get_org_from_course_url {on_cluster}
         AS (
         course_url) ->
         nullIf(EXTRACT(course_url, 'course-v1:([a-zA-Z0-9]*)'), '');
@@ -28,4 +28,4 @@ def upgrade():
 
 
 def downgrade():
-    op.execute("DROP FUNCTION IF EXISTS get_org_from_course_url;")
+    op.execute(f"DROP FUNCTION IF EXISTS get_org_from_course_url {on_cluster};")
