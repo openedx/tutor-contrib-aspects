0466fdb34d117a05d5fee53f09d1a87e607acf46
Sara Burns <sburns@axim.org>
2024-09-04 14:43:12 -0400

    feat: new course comparison dashboard

---
+++ tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
@@ -0,0 +1,14 @@
+select
+    names.org as org,
+    names.course_key as course_key,
+    names.course_name as course_name,
+    names.course_run as course_run,
+    actor_id,
+    video_id,
+    video_duration,
+    watched_time,
+    rewatched_time
+from {{ ASPECTS_XAPI_DATABASE }}.watched_video_duration
+left join {{ ASPECTS_EVENT_SINK_DATABASE }}.course_names as names
+using org, course_key
+where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
