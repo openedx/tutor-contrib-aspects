99834f2a6c48cdedfea6fd36431ac62feda8656b
Sara Burns <sburns@axim.org>
2024-09-11 11:53:32 -0400

    fix: updatse

--- tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
+++ tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
@@ -8,8 +8,7 @@ select
     video_duration,
     watched_time,
     rewatched_time
-from {% endraw -%} {{ ASPECTS_EVENT_SINK_DATABASE }}.watched_video_duration{%- raw %}
-left join
-    {% endraw -%} {{ ASPECTS_EVENT_SINK_DATABASE }}.course_names{%- raw %} as names
+from {{ ASPECTS_EVENT_SINK_DATABASE }}.watched_video_duration
+left join {{ ASPECTS_EVENT_SINK_DATABASE }}.course_names as names
 using org, course_key
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
