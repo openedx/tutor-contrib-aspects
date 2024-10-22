f6497d1d793d2f90f2e64a1c86c1858df102c235
Sara Burns <sburns@axim.org>
2024-10-22 15:19:20 -0400

    fix: video count

--- tutoraspects/templates/openedx-assets/queries/tag_filter_dataset.sql
+++ tutoraspects/templates/openedx-assets/queries/tag_filter_dataset.sql
@@ -1,11 +1,15 @@
-SELECT id, rownum, tag, course_key
-FROM (
-    SELECT 
-        id, 
-        multiIf(parent = 0, id, CAST(parent, 'int')) AS sort_order_1, 
-        multiIf(parent = 0, 0, 1) AS sort_order_2, 
-        concat(repeat('- ', countMatches(lineage, ',')), value) AS tag, 
-        row_number() OVER (ORDER BY sort_order_1 ASC, sort_order_2 ASC, value ASC) AS rownum 
-    FROM {{ ASPECTS_EVENT_SINK_DATABASE }}.most_recent_tags
-) as t
-left join {{ DBT_PROFILE_TARGET_DATABASE }}.most_recent_course_tags ct on ct.tag_id = t.id
+select id, rownum, tag, course_key
+from
+    (
+        select
+            id,
+            multiIf(parent = 0, id, CAST(parent, 'int')) as sort_order_1,
+            multiIf(parent = 0, 0, 1) as sort_order_2,
+            concat(repeat('- ', countMatches(lineage, ',')), value) as tag,
+            row_number() over (
+                order by sort_order_1 ASC, sort_order_2 ASC, value ASC
+            ) as rownum
+        from {{ ASPECTS_EVENT_SINK_DATABASE }}.most_recent_tags
+    ) as t
+left join
+    {{ DBT_PROFILE_TARGET_DATABASE }}.most_recent_course_tags ct on ct.tag_id = t.id
