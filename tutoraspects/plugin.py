4f466ea699643fd9dae5d97f3df5dfc51dac2970
Sara Burns <sburns@axim.org>
2024-11-07 13:55:35 -0500

    feat: update dbt and use new param view

--- tutoraspects/plugin.py
+++ tutoraspects/plugin.py
@@ -385,7 +385,7 @@ hooks.Filters.CONFIG_DEFAULTS.add_items(
         # For now we are pulling this from github, which should allow maximum
         # flexibility for forking, running branches, specific versions, etc.
         ("DBT_REPOSITORY", "https://github.com/openedx/aspects-dbt"),
-        ("DBT_BRANCH", "v3.33.5"),
+        ("DBT_BRANCH", "v3.33.7"),
         ("DBT_SSH_KEY", ""),
         ("DBT_STATE_DIR", "/app/aspects-dbt/state"),
         ("DBT_PROFILES_DIR", "/app/aspects/dbt/"),
--- tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
+++ tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
@@ -8,7 +8,15 @@ select
     video_duration,
     watched_time,
     rewatched_time
-from {{ ASPECTS_XAPI_DATABASE }}.watched_video_duration
-left join {{ ASPECTS_EVENT_SINK_DATABASE }}.course_names as names
-using org, course_key
+from
+    {{ DBT_PROFILE_TARGET_DATABASE }}.watched_video_duration(
+        {% raw -%}
+        org_filter ={{ filter_values("org") }},
+        course_name_filter ={{ filter_values("course_name") }}
+        {%- endraw %}
+    ) as a
+left join
+    {{ ASPECTS_EVENT_SINK_DATABASE }}.course_names as names
+    on a.org = names.org
+    and a.course_key = names.course_key
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
