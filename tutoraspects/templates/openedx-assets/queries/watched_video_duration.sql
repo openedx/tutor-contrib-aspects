7839b65a44e5198b7ca9ce8e4c72cc95c1be44b1
Sara Burns <sburns@axim.org>
2024-11-18 12:28:17 -0500

    fix: video engagement updates

--- tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
+++ tutoraspects/templates/openedx-assets/queries/watched_video_duration.sql
@@ -1,3 +1,24 @@
+{% raw -%}
+with
+    course_keys as (
+        select [] as course_key
+        {% if filter_values("course_name") != [] %}
+            union all
+            select array(course_key) as course_key
+            from {% endraw -%} {{ ASPECTS_EVENT_SINK_DATABASE }}.course_names {% raw -%}
+            where course_name in {{ filter_values("course_name") | where_in }}
+        {% endif %}
+        {% if filter_values("tag") != [] %}
+            union distinct
+            select array(course_key) as course_key
+            from
+                {% endraw -%} {{ DBT_PROFILE_TARGET_DATABASE }}.most_recent_course_tags {% raw -%}
+            where
+                tag
+                in (select replaceAll(arrayJoin({{ filter_values("tag") }}), '- ', ''))
+        {% endif %}
+    )
+    {%- endraw %}
 select
     names.org as org,
     names.course_key as course_key,
@@ -7,12 +28,14 @@ select
     video_count,
     video_duration,
     watched_time,
-    rewatched_time
+    rewatched_time,
+    object_id
 from
     {{ DBT_PROFILE_TARGET_DATABASE }}.watched_video_duration(
         {% raw -%}
-        org_filter ={{ filter_values("org") }},
-        course_name_filter ={{ filter_values("course_name") }}
+        org_filter = coalesce({{ filter_values("org") }}, []),
+        course_key_filter
+        = coalesce((select array_concat_agg(course_key) from course_keys), [])
         {%- endraw %}
     ) as a
 left join
