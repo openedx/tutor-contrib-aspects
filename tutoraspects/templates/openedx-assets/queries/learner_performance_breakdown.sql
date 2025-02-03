7295cc2874e34a9b432cd859577904c59bc47794
Sara Burns <sburns@axim.org>
2025-02-03 10:29:08 -0500

    fix: format

--- tutoraspects/templates/openedx-assets/queries/learner_performance_breakdown.sql
+++ tutoraspects/templates/openedx-assets/queries/learner_performance_breakdown.sql
@@ -15,8 +15,8 @@ select
         else 'other'
     end as learner_status,
     fss.course_key as course_key,
-    concat(course_run,' - ',course_name) as run_name,
-    concat(course_name,' - ',org) as name_org
+    concat(course_run, ' - ', course_name) as run_name,
+    concat(course_name, ' - ', org) as name_org
 from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_student_status fss
 left join {{ ASPECTS_XAPI_DATABASE }}.fact_learner_last_course_visit fllcv
 using org, course_key, actor_id
