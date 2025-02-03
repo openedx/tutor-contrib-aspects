d3c40f6f2009cf11b8e25e6c28a2651e82667526
Sara Burns <sburns@axim.org>
2025-02-03 10:26:28 -0500

    feat: add course name and org

--- tutoraspects/templates/openedx-assets/queries/learner_performance_breakdown.sql
+++ tutoraspects/templates/openedx-assets/queries/learner_performance_breakdown.sql
@@ -14,7 +14,9 @@ select
         then 'at-risk'
         else 'other'
     end as learner_status,
-    fss.course_key as course_key
+    fss.course_key as course_key,
+    concat(course_run,' - ',course_name) as run_name,
+    concat(course_name,' - ',org) as name_org
 from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_student_status fss
 left join {{ ASPECTS_XAPI_DATABASE }}.fact_learner_last_course_visit fllcv
 using org, course_key, actor_id
