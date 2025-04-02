4328b1dca3737ec1329a825386b009bddbefd361
Farhaan Bukhsh <farhaan@opencraft.com>
2025-04-02 13:52:24 +0530

    hack: commented out the create asset script
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- tutoraspects/templates/aspects/apps/superset/scripts/import-assets.sh
+++ tutoraspects/templates/aspects/apps/superset/scripts/import-assets.sh
@@ -11,8 +11,8 @@ mkdir superset
 date=$(date -u +"%Y-%m-%dT%H:%M:%S.%6N+00:00")
 echo "version: 1.0.0
 type: assets
-timestamp: '$date'" > superset/metadata.yaml
+timestamp: '$date'" >superset/metadata.yaml
 
-python /app/pythonpath/create_assets.py
+# python /app/pythonpath/create_assets.py
 
 rm -rf /app/assets/superset
