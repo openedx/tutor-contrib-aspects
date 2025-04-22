d9d171f6f7eb93788901759a5261460280c7b1ee
Sara Burns <sburns@axim.org>
2025-04-22 12:02:11 -0400

    feat: Use new dbt models, various cleanup

--- tutoraspects/templates/openedx-assets/queries/at_risk_problem_engagement.sql
+++ tutoraspects/templates/openedx-assets/queries/at_risk_problem_engagement.sql
@@ -1,15 +1,7 @@
-with
-    problem_engagement_sql as (
-        {% include 'openedx-assets/queries/problem_engagement.sql' %}
-    )
-select problem_engagement_sql.*
-from problem_engagement_sql pe
+select distinct fact_problem_engagement.*
+from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_problem_engagement
 join
     (
         {% include 'openedx-assets/queries/at_risk_learner_filter.sql' %}
-    ) as at_risk_learners
-    on (
-        pe.org = at_risk_learners.org
-        and pe.course_key = at_risk_learners.course_key
-        and pe.actor_id = at_risk_learners.actor_id
-    )
+    ) as at_risk_learners using (org, course_key, actor_id)
+where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
