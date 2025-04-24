22385be6c6ba37ec2bd40d17ae4c05c6bfc053a3
Sara Burns <sburns@axim.org>
2025-04-24 14:52:54 -0400

    fix: use problem instead of subsection

--- tutoraspects/templates/openedx-assets/queries/graded_subsection_performance.sql
+++ tutoraspects/templates/openedx-assets/queries/graded_subsection_performance.sql
@@ -5,20 +5,7 @@ with
             last_response.course_key as course_key,
             last_response.actor_id as actor_id,
             subsection_blocks.block_id as block_id,
-            last_response.scaled_score as scaled_score,
-            splitByString(
-                ' - ', subsection_blocks.display_name_with_location
-            ) as _subsection_with_name,
-            arrayStringConcat(
-                arrayMap(
-                    x -> (leftPad(x, 2, char(917768))),
-                    splitByString(':', _subsection_with_name[1])
-                ),
-                ':'
-            ) as subsection_number,
-            concat(
-                subsection_number, ' - ', _subsection_with_name[2]
-            ) as subsection_with_name
+            last_response.scaled_score as scaled_score
         from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_learner_last_response last_response
         join
             {{ DBT_PROFILE_TARGET_DATABASE }}.dim_course_blocks problem_blocks
@@ -41,20 +28,18 @@ with
             course_key,
             block_id,
             avg(scaled_score) as avg_score,
-            actor_id,
-            subsection_with_name
+            actor_id
         from section_data
-        group by org, course_key, block_id, actor_id, subsection_with_name
+        group by org, course_key, block_id, actor_id
     ),
     avg_total as (
         select
             org,
             course_key,
             block_id,
-            avg(scaled_score) as total_avg,
-            subsection_with_name
+            avg(scaled_score) as total_avg
         from section_data
-        group by org, course_key, block_id, subsection_with_name
+        group by org, course_key, block_id
     ),
     combine as (
         select
@@ -63,9 +48,16 @@ with
             avg_actor.block_id as block_id,
             round(avg_actor.avg_score * 100, 2) as avg_score,
             round(avg_total.total_avg, 2) as total_avg,
-            subsection_with_name
+            case 
+              when avg_score > 90 then '>90%'
+              when avg_score > 70 then '71-90%'
+              when avg_score > 50 then '51-70%'
+              when avg_score > 30 then '31-50%'
+              when avg_score > 0 then '1-30%'
+              else '0%'
+              end as score_range
         from avg_actor
         join avg_total using (org, course_key, block_id)
     )
-select org, course_key, block_id, avg_score, total_avg, subsection_with_name
+select org, course_key, block_id, avg_score, total_avg, score_range
 from combine
--- tutoraspects/templates/openedx-assets/queries/graded_subsection_results.sql
+++ tutoraspects/templates/openedx-assets/queries/graded_subsection_results.sql
@@ -1,47 +1,56 @@
 with
-    last_response as (
-        select
-            org,
-            course_key,
-            actor_id,
-            object_id,
-            problem_id,
-            argMax(success, attempts) as success,
-            MAX(attempts) as last_attempt
-        from problem_events
-        group by org, course_key, actor_id, object_id, problem_id
-    ),
-    problem_results as (
+    final_results as (
         select
             last_response.org as org,
             last_response.course_key as course_key,
             last_response.actor_id as actor_id,
-            blocks.subsection_number as subsection_number,
-            last_response.problem_id as problem_id,
-            blocks.block_name as problem_name,
-            last_response.success,
-            blocks.course_order
-        from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_course_blocks blocks
+            subsection_blocks.block_id as subsection_block_id,
+            problem_blocks.block_id as problem_id,
+            last_response.success as success,
+            substring(
+                regexpExtract(
+                    first_success.object_id, '(@problem+block@[wd][^_/]*)(_d)?', 2
+                ),
+                2
+            ) as _problem_id_number,
+            cast(
+                ifNull(nullIf(_problem_id_number, ''), '1') as Int
+            ) as _problem_id_or_1,
+            splitByString(
+                ' - ', problem_blocks.display_name_with_location
+            ) as _problem_with_name,
+            arrayStringConcat(
+                arrayMap(
+                    x -> (leftPad(x, 2, char(917768))),
+                    splitByString(':', _problem_with_name[1])
+                ),
+                ':'
+            ) as _problem_number,
+            concat(_problem_number, '_', _problem_id_or_1) as problem_number,
+            concat(
+                problem_number, ' - ', _problem_with_name[2]
+            ) as problem_name_location
+        from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_learner_last_response last_response
         join
-            last_response
-            on blocks.block_id = last_response.problem_id
-            and last_response.course_key = blocks.course_key
-        where blocks.graded
+            {{ DBT_PROFILE_TARGET_DATABASE }}.dim_course_blocks problem_blocks
+            on problem_blocks.block_id = last_response.problem_id
+            and last_response.course_key = problem_blocks.course_key
+        left join
+            {{ DBT_PROFILE_TARGET_DATABASE }}.dim_course_blocks subsection_blocks
+            on problem_blocks.subsection_number = subsection_blocks.hierarchy_location
+            and last_response.org = subsection_blocks.org
+            and last_response.course_key = subsection_blocks.course_key
+            and (
+                subsection_blocks.block_id like '%@sequential+block@%'
+                or subsection_blocks.block_id like '%@chapter+block@%'
+            )
+        where problem_blocks.graded
     )
 select
-    ips.org as org,
-    ips.course_key as course_key,
-    splitByString('@', subsection_blocks.block_id)[-1] as block_id,
-    ips.subsection_number as subsection_number,
-    subsection_blocks.display_name_with_location as subsection_with_name,
-    problem_id,
-    problem_name,
+    org,
+    course_key,
+    subsection_block_id as block_id,
+    problem_number,
     actor_id,
     success
-from problem_results ips
-left join
-    {{ DBT_PROFILE_TARGET_DATABASE }}.dim_course_blocks subsection_blocks
-    on ips.subsection_number = subsection_blocks.hierarchy_location
-    and ips.org = subsection_blocks.org
-    and ips.course_key = subsection_blocks.course_key
-    and subsection_blocks.block_id like '%@sequential+block@%'
+from final_results
