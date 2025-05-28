f72196c5a1c7231496406fd94775fbca4f482269
Sara Burns <sburns@axim.org>
2025-05-28 13:06:34 -0400

    feat: Updates to video queries
    fix: add distinct
    
    fix: remove duplicates
    
    fix: use watch count
    
    fix: rework video segments
    
    fix: rename dicts on alembic downgrade
    
    fix: sum of response count
    
    fix: update dbt version
    
    fix: reset dashboard locales

--- tutoraspects/templates/openedx-assets/queries/video_watches.sql
+++ tutoraspects/templates/openedx-assets/queries/video_watches.sql
@@ -1,51 +1,51 @@
 with
-    watched_segments as (
+    watched_video_segments as (
         {% include 'openedx-assets/queries/watched_video_segments.sql' %}
+    ),
+    final_results as (
+        select
+            org,
+            course_key,
+            video_name_location,
+            video_link,
+            actor_id,
+            video_duration,
+            username,
+            email,
+            name,
+            count(segment_start) as _total_segments_watched,
+            max(watched_count) as video_watched_count,
+            max(
+                case when watched_count > 1 then watched_count else 0 end
+            ) as video_rewatched_count,
+            section_with_name,
+            subsection_with_name
+        from watched_video_segments
+        group by
+            org,
+            course_key,
+            video_name_location,
+            video_link,
+            actor_id,
+            username,
+            email,
+            name,
+            video_duration,
+            section_with_name,
+            subsection_with_name
     )
-
 select
     org,
     course_key,
-    course_name,
-    course_run,
-    section_with_name,
-    subsection_with_name,
-    video_name,
-    video_name_with_location,
+    video_name_location,
     video_link,
     actor_id,
     username,
     email,
     name,
-    count(distinct segment_start) as watched_segment_count,
-    (video_duration - 10) / 5 as video_segment_count,
-    video_segment_count <= watched_segment_count as watched_entire_video
-from watched_segments
-where
-    1 = 1
-    {% raw %}
-    {% if filter_values("Section Name") != [] %}
-        and section_with_name in {{ filter_values("Section Name") | where_in }}
-    {% endif %}
-    {% if filter_values("Subsection Name") != [] %}
-        and subsection_with_name in {{ filter_values("Subsection Name") | where_in }}
-    {% endif %}
-    {% if from_dttm %} and started_at > '{{ from_dttm }}' {% endif %}
-    {% if to_dttm %} and started_at < '{{ to_dttm }}' {% endif %}
-    {% endraw %}
-group by
-    org,
-    course_key,
-    course_name,
-    course_run,
+    video_watched_count,
+    video_rewatched_count,
+    _total_segments_watched / video_duration >= .95 as watched_entire_video,
     section_with_name,
-    subsection_with_name,
-    video_name,
-    video_name_with_location,
-    video_link,
-    actor_id,
-    video_id,
-    video_segment_count,
-    username,
-    email,
-    name
+    subsection_with_name
+from final_results
