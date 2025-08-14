bc874cbfec0b2553be071ebe2806761bfdf64bc6
Sara Burns <sburns@axim.org>
2025-08-14 12:22:10 -0400

    wip

--- tutoraspects/templates/openedx-assets/queries/navigation_completion.sql
+++ tutoraspects/templates/openedx-assets/queries/navigation_completion.sql
@@ -1,3 +1,3 @@
 select *
-from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_navigation
+from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_navigation_completion
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
