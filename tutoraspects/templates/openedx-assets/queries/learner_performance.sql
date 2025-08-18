af21a561cf85252a5fad4cd4b9afebf5d7dc7d69
Sara Burns <sburns@axim.org>
2025-08-18 14:59:36 -0400

    wip

--- tutoraspects/templates/openedx-assets/queries/learner_performance.sql
+++ tutoraspects/templates/openedx-assets/queries/learner_performance.sql
@@ -1,22 +1,20 @@
 select
-    last_response.org as org,
-    last_response.course_key as course_key,
+    status.org as org,
+    status.course_key as course_key,
     blocks.course_name as course_name,
     blocks.course_run as course_run,
-    last_response.actor_id as actor_id,
-    last_response.success as success,
-    last_response.attempt as attempts,
+    status.actor_id as actor_id,
+    sum(case when success and attempt = 1 then 1 else 0 end) as first_try_correct_count,
+    count(distinct case when attempt > 1 then last_response.object_id end) as response_count,
     status.course_grade as course_grade,
     status.approving_state as approving_state
-from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_learner_last_response as last_response
+from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_student_status as status
 join
-    {{ DBT_PROFILE_TARGET_DATABASE }}.dim_course_blocks blocks
-    on (
-        last_response.course_key = blocks.course_key
-        and last_response.problem_id = blocks.block_id
-    )
-join {{ DBT_PROFILE_TARGET_DATABASE }}.dim_student_status status
+    {{ ASPECTS_EVENT_SINK_DATABASE }}.dim_course_names blocks using (course_key)
+left join
+    {{ DBT_PROFILE_TARGET_DATABASE }}.dim_learner_last_response as last_response
     on status.org = last_response.org
     and status.course_key = last_response.course_key
     and status.actor_id = last_response.actor_id
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
+group by org, course_key, course_name, course_run, actor_id,course_grade, approving_state
