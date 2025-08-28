85d17bbd535ddd26578fddccd2ab786d87f33340
Sara Burns <sburns@axim.org>
2025-08-28 11:50:07 -0400

    wip

--- tutoraspects/templates/openedx-assets/queries/at_risk_problem_engagement.sql
+++ tutoraspects/templates/openedx-assets/queries/at_risk_problem_engagement.sql
@@ -3,9 +3,5 @@ from {{ DBT_PROFILE_TARGET_DATABASE }}.fact_problem_engagement
 join
     (
         {% include 'openedx-assets/queries/at_risk_learner_filter.sql' %}
-    ) as at_risk_learners
-    on (
-        fact_problem_engagement.org = at_risk_learners.org
-        and fact_problem_engagement.course_key = at_risk_learners.course_key
-        and fact_problem_engagement.actor_id = at_risk_learners.actor_id
-    )
+    ) as at_risk_learners using (org, course_key, actor_id)
+where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
