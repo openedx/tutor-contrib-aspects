39be3dcc86fe5fcc774a47f3ef062f0e12fac7f2
Sara Burns <sburns@axim.org>
2025-09-23 14:05:23 -0400

    wip

--- tutoraspects/templates/openedx-assets/queries/enrollment_status.sql
+++ tutoraspects/templates/openedx-assets/queries/enrollment_status.sql
@@ -1,15 +1,14 @@
 select
-    enrollment.org,
-    enrollment.course_key,
-    enrollment.actor_id,
-    enrollment.enrollment_status,
-    enrollment.enrollment_mode,
-    enrollment.emission_time,
-    names.course_name,
-    names.course_run
+    enrollment.emission_time as emission_time,
+    enrollment.org as org,
+    enrollment.course_key as course_key,
+    names.course_name as course_name,
+    names.course_run as course_run,
+    enrollment.actor_id as actor_id,
+    enrollment.enrollment_mode as enrollment_mode,
+    enrollment.enrollment_status as enrollment_status
 from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_most_recent_enrollment enrollment
-left join
+join
     {{ ASPECTS_EVENT_SINK_DATABASE }}.dim_course_names names
-    on enrollment.org = names.org
-    and enrollment.course_key = names.course_key
+    on enrollment.course_key = names.course_key
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
--- tutoraspects/templates/openedx-assets/queries/student_status.sql
+++ tutoraspects/templates/openedx-assets/queries/student_status.sql
@@ -1,13 +1,3 @@
-with
-    recent_activity as (
-        select course_key, COUNT(DISTINCT actor_id) as active_last_7_days
-        from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_learner_last_course_visit
-        where
-            1 = 1
-            {% include 'openedx-assets/queries/common_filters.sql' %}
-            and emission_time >= subtractDays(now(), 7)
-        group by course_key
-    )
 select
     status.org as org,
     status.course_key as course_key,
@@ -22,12 +12,10 @@ select
     status.email as email,
     status.enrolled_at as enrolled_at,
     status.last_visited as last_visited,
-    COALESCE(recent_activity.active_last_7_days, 0) as active_within_last_7_days,
     names.course_name as course_name,
     names.course_run as course_run
 from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_student_status status
 join
     {{ ASPECTS_EVENT_SINK_DATABASE }}.dim_course_names names
     on status.course_key = names.course_key
-left join recent_activity on status.course_key = recent_activity.course_key
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
