8b1ee28fbf47568d93876ffad5155cfc14022e8f
Sara Burns <sburns@axim.org>
2026-06-26 14:00:05 -0400

    fix: Aspects updates jun17 (#1273)
    * fix: use problem results dataset instead of performance
    
    * chore: Update tutor dependency to use Git URL
    
    * chore: Update DBT repository and branch settings

--- tutoraspects/templates/openedx-assets/queries/graded_subsection_performance.sql
+++ tutoraspects/templates/openedx-assets/queries/graded_subsection_performance.sql
@@ -1,9 +1,22 @@
 select
-    performance.org as org,
-    performance.course_key as course_key,
-    performance.block_id as block_id,
-    performance.total_avg as total_avg,
-    performance.score_range as score_range,
-    performance.score_range_count as score_range_count
-from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_subsection_performance performance
+    results.org as org,
+    results.course_key as course_key,
+    results.block_id_short as block_id,
+    results.actor_id as actor_id,
+    case
+        when round(avg(results.scaled_score) * 100, 2) > 90
+        then '>90%'
+        when round(avg(results.scaled_score) * 100, 2) > 70
+        then '71-90%'
+        when round(avg(results.scaled_score) * 100, 2) > 50
+        then '51-70%'
+        when round(avg(results.scaled_score) * 100, 2) > 30
+        then '31-50%'
+        when round(avg(results.scaled_score) * 100, 2) > 0
+        then '1-30%'
+        else '0%'
+    end as score_range,
+    avg(results.scaled_score) as avg_score
+from {{ DBT_PROFILE_TARGET_DATABASE }}.dim_subsection_problem_results results
 where 1 = 1 {% include 'openedx-assets/queries/common_filters.sql' %}
+group by results.org, results.course_key, results.block_id_short, results.actor_id
